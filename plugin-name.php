f4e7cb611b38a487e930464f853914aae941a849
k3x4 <xkexagias@gmail.com>
2018-08-27 13:19:40 +0300

    First commit

---
+++ plugin-name.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Plugin Name:       Plugin name
+ * Plugin URI:        
+ * Description:       
+ * Version:           1.0.0
+ * Author:            k3x4
+ * Author URI:        
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ */
+
+namespace k3x4\PluginName;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// We load Composer's autoload file
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+register_activation_hook( __FILE__, function(){
+	utils\Activator::activate();
+} );
+
+register_deactivation_hook( __FILE__, function() {
+	utils\Deactivator::deactivate();
+} );
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since 1.0.0
+ */
+
+$plugin = new Main();
+$plugin->run();
